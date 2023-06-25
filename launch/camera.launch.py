012ed0f9d210e921440740c613650adad3f43098
Ervin Picardal <ervinpicardal@pop-os.localdomain>
2023-06-25 09:18:34 +0800

    Updated URDF and added ros2_control

---
+++ launch/camera.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+   
+   camera_config = os.path.join(get_package_share_directory('robot_bringup'), 'config', 'camera.yaml')
+   
+   camera_node = Node(     
+      package="v4l2_camera",
+      executable="v4l2_camera_node",
+      output="screen",
+      parameters=[camera_config],
+   )
+      
+   rqt_image_viewer = Node(
+      package="rqt_image_view",
+      executable="rqt_image_view",
+      output="screen"
+   )
+   
+   return LaunchDescription([
+      camera_node,
+      rqt_image_viewer
+   ])
+   
+   
+   
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,13 +3,16 @@ import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
    
+   use_ros2_control = LaunchConfiguration('use_ros2_control')
    use_sim_time = LaunchConfiguration('use_sim_time')
+   
    
    pkg_directory = get_package_share_directory('robot_bringup')
    gazebo_directory = get_package_share_directory('gazebo_ros')
@@ -17,14 +20,16 @@ def generate_launch_description():
    rsp = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([os.path.join(
          pkg_directory, 'launch', 'rsp.launch.py'
-      )]), launch_arguments={'use_sim_time': 'true'}.items()
+      )]), launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': use_ros2_control}.items()
    )
    
+   gazebo_params_file = os.path.join(pkg_directory,'config','gazebo_params.yaml')
+   
    gazebo = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([os.path.join(
-         gazebo_directory, 'launch', 'gazebo.launch.py'
-      )]), launch_arguments={'use_sim_time': 'true'}.items()
-   )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
    
    spawn_entity_node = Node(
       package="gazebo_ros",
@@ -34,8 +39,43 @@ def generate_launch_description():
       output='screen'
    )
    
+   diff_cont_controller = Node(
+      package="controller_manager",
+      executable="spawner",
+      arguments=['diff_cont']
+   )
+   
+   joint_broad_controller = Node(
+      package="controller_manager",
+      executable="spawner",
+      arguments=['joint_broad']
+   )
+   
    return LaunchDescription([
+      RegisterEventHandler(
+         event_handler=OnProcessExit(
+            target_action=spawn_entity_node,
+            on_exit=[joint_broad_controller]
+         )
+      ),
+      
+      RegisterEventHandler(
+         event_handler=OnProcessExit(
+            target_action=joint_broad_controller,
+            on_exit=[diff_cont_controller]
+         )
+      ),
+      
+      # RegisterEventHandler(
+      #    event_handler=OnProcessExit(
+      #       target_action=diff_cont_controller,
+      #       on_exit=[]
+      #    )
+      # ),
+      
+      
       rsp,
       gazebo,
-      spawn_entity_node
+      spawn_entity_node,
+      
    ])
